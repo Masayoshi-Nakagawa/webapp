f5039a219443b020aa3c129c618284fa890ab4d3
Masayoshi-Nakagawa <102711095+Masayoshi-Nakagawa@users.noreply.github.com>
2024-02-10 19:58:06 +0900

    Update navigation1.py

--- navigation1.py
+++ navigation1.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
 import os
-
+from PIL import Image
 
 image = Image.open('logo_novel1.png')
 st.image(image)
